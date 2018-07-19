3479d8b83af7997079cf6d7f8e3775891cbb7425
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-19 14:46:27 -0400

    Added Gallery files

--- Spotify-api-intro/breezify/src/App.jsx
+++ Spotify-api-intro/breezify/src/App.jsx
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {FormGroup, FormControl, InputGroup, Glyphicon,Button} from 'react-bootstrap';
+import { FormGroup, FormControl, InputGroup, Glyphicon, Button } from 'react-bootstrap';
 
 import Profile from './Profile'
+import Gallery from './Gallery'
 
 
 //Spotify API
@@ -11,19 +12,18 @@ const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
-     
-      this.state = {
-        
-        nowPlaying: { name: 'Not Checked',
-         albumArt: '',
-         query: '',
-         artist: ''  // my response.
-        
-        }
-      }
+
+    this.state = {
+      
+        query: '',
+        artist: null,  // my response.
+        tracks: []
+
+      
+    }
 
 
   }
@@ -36,8 +36,10 @@ class App extends Component {
 
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
-    const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
-    var accessToken = 'BQBNeF-h6QHmDSFpyoRYqYtT6RpRa-2Ze2yzA4aIYUGe9Q4voxj5TeAVWf6CHAB_J8TRqgSzq4zaK_Jt0SOtwumuXgpEEQPx5HaIaG27KIEVk8y3Y2s359cfT-XZxQHbQ-k39zkOmfV4rxDGUnnOkYH9XWgQi7PntzqOLnV00tdMppYeObD2ux8&refresh_token=AQDzHQE0RvBDAwCN_4b-sjNksn3glwloMPfIpv5r2abMcXYNVVnnjLRKi3DZyZC_uT9RjxMm8t6YNIgO_vmRwELByC3RmvlpbDRQcFUy6I0ZBt--i_londwxBv-5KMQkjQM'
+    let FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    var accessToken = 'BQDuPqnQz8BeN5IS2KlB2fAkGZfGsqsAg_9ZWucwhugSSVAztjj6s2Qyq7Bx6ZW-tKZN_nVyYnqGT7ogLA2CgX1MNbqU9l5nXSCOoWWxWMRdXqGmLprcghLzlHiXDAbwnKIWOILxrTNgtna5nJXFK3K8I3DxFTrPaBmNkvRdmZ-Mkzh66d4GHig&refresh_token=AQBa3oCHRL3ss3W1IkNDz1GTS5by5opr6GwsGgCFn48XsmKXRxrFfVcsiluJctP_ddawQFae3SoKUw3JozpG3fDFBhUeIH6uV1Xv8wKRhz-Ctu4_ak8FVs7_opzhy8FN-b4'
+
+    const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
 
     var myToken = {
       method: 'GET',
@@ -48,12 +50,23 @@ class App extends Component {
       cache: 'default'
     };
     fetch(FETCH_URL, myToken)
-    .then(response => response.json())
-    .then(json => {
-      const artist = json.artists.items[0];        
-      this.setState({ artist });
-    })
-      
+      .then(response => response.json())
+      .then(json => {
+        const artist = json.artists.items[0];
+        this.setState({ artist });
+
+
+
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+        fetch(FETCH_URL, myToken)
+          .then(response => response.json())
+          .then(json => {
+            console.log('artist\'s top tracks:', json);
+            const { tracks } = json;
+            this.setState({ tracks });
+          })
+      })
+
 
   }
 
@@ -61,49 +74,50 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">  
-      <div className="App-title">
-                  Breezify MusicApp
-                </div>
-                 
-                 <FormGroup>
-
-                     <InputGroup>
-
-                     <FormControl
-                     type="text"
-                     placeholder="Search for an Artist" 
-                     value={this.state.query}
-                     onChange ={event => {this.setState({query: event.target.value})}}
-                     onKeyPress ={event =>
-                        {
-                            if(event.key ==='Enter')
-                            {
-                                this.search()
-                            }
-                        } 
-                    }
-                     />
-                     
-                     
-                     <InputGroup.Addon onClick={() => this.search()}>
-                              <Glyphicon glyph="search"></Glyphicon>
-                    </InputGroup.Addon>
-
-                     </InputGroup>
-                 </FormGroup>
-
-                  <Profile
-
-                  artist ={this.state.artist}
-                  
-                  />
-
-                <div className="Gallery">
-                    Gallery
+      <div className="App">
+        <div className="App-title">
+          Breezify MusicApp
                 </div>
 
-    </div>
+        <FormGroup>
+
+          <InputGroup>
+
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={event => { this.setState({ query: event.target.value }) }}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search()
+                }
+              }
+              }
+            />
+
+
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+
+          </InputGroup>
+        </FormGroup>
+        {
+          this.state.artist !== null
+            ?
+            <div>
+              <Profile
+                artist={this.state.artist}
+              />
+              <Gallery
+                tracks={this.state.tracks}
+              />
+            </div>
+            : <div></div>
+        }
+
+      </div>
     );
   }
 }
